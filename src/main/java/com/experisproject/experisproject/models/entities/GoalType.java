6eb6973a5f105e1db06d8c2548572fedd605a90d
karolinr9 <klrykkelid@outlook.com>
2018-10-15 14:59:09 +0200

    add goalType entity

---
+++ src/main/java/com/experisproject/experisproject/models/entities/GoalType.java
@@ -0,0 +1,14 @@
+package com.experisproject.experisproject.models.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class GoalType {
+	@Id
+	@GeneratedValue
+	private Long goalTypeId;
+
+	private String type;
+}
