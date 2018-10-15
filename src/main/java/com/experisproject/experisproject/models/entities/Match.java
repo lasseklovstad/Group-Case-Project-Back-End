112e7c5356ce510745ff7f091a6b68ce0ccf0cca
karolinr9 <klrykkelid@outlook.com>
2018-10-15 15:46:15 +0200

    add match entity (without foreign keys)

---
+++ src/main/java/com/experisproject/experisproject/models/entities/Match.java
@@ -0,0 +1,25 @@
+package com.experisproject.experisproject.models.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class Match {
+	@Id
+	@GeneratedValue
+	private int matchId;
+
+	private LocalDate matchDate;
+
+    //  season_id INT NOT NULL,
+	//  location_id INT NOT NULL,
+	//  home_team_id INT NOT NULL,
+	//  away_team_id INT NOT NULL,
+	//  PRIMARY KEY (match_id),
+	//  FOREIGN KEY (season_id) REFERENCES SEASON(season_id),
+	//  FOREIGN KEY (location_id) REFERENCES LOCATION(location_id),
+	//  FOREIGN KEY (home_team_id) REFERENCES TEAM(team_id),
+	//  FOREIGN KEY (away_team_id) REFERENCES TEAM(team_id)
+}
