9f1ecc48d7ffc2aa6cc2bee066eee46cadde0758
karolinr9 <klrykkelid@outlook.com>
2018-10-15 15:26:55 +0200

    add contact entity (without foreign key)

---
+++ src/main/java/com/experisproject/experisproject/models/entities/Contact.java
@@ -0,0 +1,24 @@
+package com.experisproject.experisproject.models.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+@Entity
+public class Contact {
+	@Entity
+	@GeneratedValue
+	private int contactId;
+
+	private String contactType;
+	private String contactDetail;
+	// FOREIGN KEY (person_id) REFERENCES PERSON(person_id)
+
+
+	public Contact() {
+	}
+
+	public Contact(String contactType, String contactDetail) {
+		this.contactType = contactType;
+		this.contactDetail = contactDetail;
+	}
+}
