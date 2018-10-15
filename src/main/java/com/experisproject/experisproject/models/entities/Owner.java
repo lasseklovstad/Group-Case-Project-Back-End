61abb1539d59fdac75ffadf86a922106a07c728b
karolinr9 <klrykkelid@outlook.com>
2018-10-15 15:30:56 +0200

    add owner entity (without foreign key)

---
+++ src/main/java/com/experisproject/experisproject/models/entities/Owner.java
@@ -0,0 +1,19 @@
+package com.experisproject.experisproject.models.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Owner {
+	@Id
+	@GeneratedValue
+	private int ownerId;
+
+	// PRIMARY KEY (owner_id),
+	// FOREIGN KEY (person_id) REFERENCES PERSON(person_id)
+
+
+	public Owner() {
+	}
+}
