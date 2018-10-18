f422d62759adf407b05857cb533b3b28cffc13a6
karolinr9 <klrykkelid@outlook.com>
2018-10-18 13:31:47 +0200

    "fill user class"

--- src/main/java/com/experisproject/experisproject/models/entities/User.java
+++ src/main/java/com/experisproject/experisproject/models/entities/User.java
@@ -1,4 +1,29 @@
 package com.experisproject.experisproject.models.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class User {
+	@Id
+	@GeneratedValue
+	private int userId;
+
+	@NotNull private String userName;
+	@NotNull private String email;
+	@NotNull private String password;
+	@NotNull private boolean isAdmin;
+
+	public User(@NotNull String userName, @NotNull String email, @NotNull String password, @NotNull boolean isAdmin) {
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+		this.isAdmin = isAdmin;
+
+		if (userName.equals("admin")){
+			this.isAdmin = true;
+		}
+	}
 }
