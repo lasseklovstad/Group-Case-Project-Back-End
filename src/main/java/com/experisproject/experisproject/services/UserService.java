a53a7efa9dee7430df5e36d98d6222fb41faefe0
karolinr9 <klrykkelid@outlook.com>
2018-10-18 15:52:16 +0200

    add userservice code

--- src/main/java/com/experisproject/experisproject/services/UserService.java
+++ src/main/java/com/experisproject/experisproject/services/UserService.java
@@ -1,4 +1,35 @@
 package com.experisproject.experisproject.services;
 
+import com.experisproject.experisproject.models.entities.User;
+import com.experisproject.experisproject.models.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class UserService {
+	private UserRepository userRepository;
+
+	@Autowired
+	public UserService(UserRepository userRepository){
+		this.userRepository = userRepository;
+	}
+
+	public List<User> findAll(){
+		Iterable<User> users = this.userRepository.findAll();
+		List<User> result = new ArrayList<>();
+
+		for (User user : users) {
+			result.add(user);
+		}
+
+		return result;
+	}
+
+	public void save(User user){
+		userRepository.save(user);
+	}
+
 }
