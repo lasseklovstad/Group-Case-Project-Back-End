bb818f2718e9c34af8785f1445648432c3dfd1dc
karolinr9 <klrykkelid@outlook.com>
2018-10-19 15:58:52 +0200

    add method

--- src/main/java/com/experisproject/experisproject/models/repositories/OwnerRepository.java
+++ src/main/java/com/experisproject/experisproject/models/repositories/OwnerRepository.java
@@ -1,7 +1,13 @@
 package com.experisproject.experisproject.models.repositories;
 
 import com.experisproject.experisproject.models.entities.Owner;
+import com.experisproject.experisproject.models.entities.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OwnerRepository extends CrudRepository<Owner,Integer> {
+import java.util.List;
+
+public interface OwnerRepository extends JpaRepository<Owner,Integer> {
+
+	List<Owner> findByPerson(Person person);
 }
