ea1c541418e3c5f4a00fdc2b48912b3ed337c03e
Lasse-Hjemme <lasse.klovstad@gmail.com>
2018-10-23 22:44:37 +0200

    findAlllimitedLocation

--- src/main/java/com/experisproject/experisproject/controllers/LocationController.java
+++ src/main/java/com/experisproject/experisproject/controllers/LocationController.java
@@ -1,4 +1,25 @@
 package com.experisproject.experisproject.controllers;
 
+import com.experisproject.experisproject.projections.LocationLimited;
+import com.experisproject.experisproject.services.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/seasons")
+@CrossOrigin
 public class LocationController {
+    @Autowired
+    LocationService locationService;
+
+    @RequestMapping(value = "/all",method = RequestMethod.GET)
+    public List<LocationLimited> getAllLimited(){
+        return locationService.findAllLimited();
+    }
+
 }
--- src/main/java/com/experisproject/experisproject/models/repositories/LocationRepository.java
+++ src/main/java/com/experisproject/experisproject/models/repositories/LocationRepository.java
@@ -1,7 +1,15 @@
 package com.experisproject.experisproject.models.repositories;
 
 import com.experisproject.experisproject.models.entities.Location;
+import com.experisproject.experisproject.projections.LocationLimited;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface LocationRepository extends CrudRepository<Location,Integer> {
+import java.util.List;
+
+public interface LocationRepository extends JpaRepository<Location,Integer> {
+
+    @Query(value = "SELECT l FROM Location l")
+    List<LocationLimited> findAllLimited();
 }
