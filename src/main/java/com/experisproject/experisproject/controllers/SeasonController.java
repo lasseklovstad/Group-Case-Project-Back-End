32624216d984df851104b997b1cf79cdfbc42583
Lasse.Klovstad <lasse.klovstad@gmail.com>
2018-10-24 10:14:28 +0200

    season-save-controller

--- src/main/java/com/experisproject/experisproject/controllers/SeasonController.java
+++ src/main/java/com/experisproject/experisproject/controllers/SeasonController.java
@@ -1,14 +1,16 @@
 package com.experisproject.experisproject.controllers;
 
+import com.experisproject.experisproject.models.entities.Season;
+import com.experisproject.experisproject.models.forms.SeasonForm;
 import com.experisproject.experisproject.projections.SeasonLimited;
 import com.experisproject.experisproject.services.SeasonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 @CrossOrigin
@@ -23,5 +25,12 @@ public class SeasonController {
         return seasonService.findAllLimited();
     }
 
-    @RequestMapping
+    @RequestMapping(value = "" , method = RequestMethod.POST)
+    public void createNew(@RequestBody SeasonForm form){
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        dtf=dtf.withLocale(Locale.US);
+        Season season = new Season(form.getName(),LocalDate.parse(form.getStartDate(),dtf),LocalDate.parse(form.getEndDate(),dtf),form.getDescription());
+        seasonService.save(season);
+    }
 }
--- src/main/java/com/experisproject/experisproject/services/SeasonService.java
+++ src/main/java/com/experisproject/experisproject/services/SeasonService.java
@@ -1,5 +1,6 @@
 package com.experisproject.experisproject.services;
 
+import com.experisproject.experisproject.models.entities.Season;
 import com.experisproject.experisproject.models.repositories.SeasonRepository;
 import com.experisproject.experisproject.projections.SeasonLimited;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,10 @@ public class SeasonService {
     @Autowired
     SeasonRepository seasonRepository(SeasonRepository seasonRepository){
         return this.seasonRepository=seasonRepository;
+    }
+
+    public void save(Season season){
+        seasonRepository.save(season);
     }
 
     public List<SeasonLimited> findAllLimited(){
