565278c4d6075ab4545a528a3f406ff51d1d20d4
karolinr9 <klrykkelid@outlook.com>
2018-10-25 11:26:46 +0200

    add concat to player/personname

--- src/main/java/com/experisproject/experisproject/models/repositories/PlayerRepository.java
+++ src/main/java/com/experisproject/experisproject/models/repositories/PlayerRepository.java
@@ -11,10 +11,9 @@ public interface PlayerRepository extends JpaRepository<Player, Integer> {
 
 	List<Player> findPlayerByTeam_NameContaining(String name);
 
-	@Query(value = "SELECT p.playerId as playerId, p.person.firstName as firstName, p.person.lastName as lastName, p.team.name as team FROM Player p")
+	@Query(value = "SELECT p.playerId as playerId, CONCAT(p.person.firstName,' ',p.person.lastName) as fullName, p.team.name as team FROM Player p")
 	List<Player> findPlayerShortInfo();
 
-	@Query(value = "SELECT pl FROM Player pl, Person pers, Team team where pl.person.personId = pers.personId AND p.team.teamId = team.teamId")
+	@Query(value = "SELECT pl FROM Player pl INNER JOIN Person pers ON pl.person.personId = pers.personId INNER JOIN Team team ON pl.team.teamId = team.teamId")
 	List<PlayerLimited> findAllLimited(); //works exactly the same
-
 }
