89243dad580fe7641ca70d718732e81a2d6fb4e1
karolinr9 <klrykkelid@outlook.com>
2018-10-26 12:43:38 +0200

    add post requestmethod, mapping, create address

--- src/main/java/com/experisproject/experisproject/controllers/AddressController.java
+++ src/main/java/com/experisproject/experisproject/controllers/AddressController.java
@@ -5,6 +5,7 @@ import com.experisproject.experisproject.services.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -12,11 +13,29 @@ import java.util.List;
 @RequestMapping(value = "api/address")
 public class AddressController {
 
-    @Autowired
-    AddressService addressService;
+	@Autowired
+	AddressService addressService;
 
-    @RequestMapping(value = "/all",method = RequestMethod.GET)
-    public List<Address> findAllShortInfo(){
-        return addressService.getAllShortInfo();
-    }
+	@RequestMapping(value = "/all", method = RequestMethod.GET)
+	public List<Address> getAllAddressInfo() {
+		return addressService.findAddressesShortInfo();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Address getAddressById(@PathVariable int id) {
+		return addressService.findById(id);
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public void createNewAddress(@RequestBody Address address, HttpServletResponse response){
+		try {
+			addressService.save(address);
+			response.setStatus(HttpServletResponse.SC_CREATED);
+
+		}catch (Exception e){
+			e.getCause();
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+		}
+	}
+
 }
