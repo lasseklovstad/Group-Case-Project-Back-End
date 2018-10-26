83f543e968a9f56bb86c2df0aae738404904b3c7
karolinr9 <klrykkelid@outlook.com>
2018-10-26 12:51:38 +0200

    change to jparepository

--- src/main/java/com/experisproject/experisproject/models/repositories/UserRepository.java
+++ src/main/java/com/experisproject/experisproject/models/repositories/UserRepository.java
@@ -1,11 +1,12 @@
 package com.experisproject.experisproject.models.repositories;
 
 import com.experisproject.experisproject.models.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 	User findByEmail(String email);
 }
