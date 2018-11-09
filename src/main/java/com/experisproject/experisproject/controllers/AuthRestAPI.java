7ec3b2376ff260db59d8acd4785071e9b84e1db1
karolinr9 <klrykkelid@outlook.com>
2018-11-09 17:29:14 +0100

    remove unused import

--- src/main/java/com/experisproject/experisproject/controllers/AuthRestAPI.java
+++ src/main/java/com/experisproject/experisproject/controllers/AuthRestAPI.java
@@ -45,8 +45,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sun.jvm.hotspot.memory.HeapBlock;
-
 
 @RestController
 @RequestMapping("/api/auth")
